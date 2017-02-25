412116bd40b8b08103a06cd0e7b80df4c9f91c8b
Artur Rabenda <rabenda27178@student.pwsztar.edu.pl>
2017-02-25 15:10:25 +0100

    Artur3

--- Osoba.java
+++ Osoba.java
@@ -15,5 +15,13 @@ public class Osoba {
     public Osoba(String imie) {
         this.imie = imie;
     }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
     
 }
